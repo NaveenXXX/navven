311465b05124a21efc11f7c0b7a0fba2a4b6753d
NaveenPpp <112401605+NaveenXXX@users.noreply.github.com>
2022-09-26 16:58:42 +0530

    Update steps.py

--- steps.py
+++ steps.py
@@ -5,8 +5,11 @@ def climbStairs(n):
  for i in range(2, n):
   steps.append(f[i - 1] + f[i - 2])
  return steps[n - 1]
-n=3
-print(climbStairs(n))
+n=int(input("enter a number"))
+if(n<0):
+     print("error")
+else:
+    print(climbStairs(n))
 
 
 
